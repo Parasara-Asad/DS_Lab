be4bfb8917da706972ec4871c8b6342526b7042f
Parasara Mohamad Asad <kingofparasara786@gmail.com>
2026-06-11 18:53:12 +0530

    lab

---
+++ Lab_2/prime_or_not_between.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+void prime_or_not(int n);
+int main(){
+    int s,e;
+    printf("Enter start number is: ");
+    scanf("%d",&s);
+    printf("Enter end number is: ");
+    scanf("%d",&e);
+    for (int i = s; i <= e; i++)
+    {
+       prime_or_not(i);
+    }
+    
+    return 0;
+}
+
+
+void prime_or_not(int n){
+    int conunt=0;
+    for (int i = 1; i <= n; i++)
+    {
+        if(n%i==0){
+            conunt++;
+        }
+        
+    }
+    if (conunt==2)
+    {
+        printf(" %d",n);
+    }
+}
